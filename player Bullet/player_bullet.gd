0cdd6ea0200e518905026908470a1801d9bd1780
Asher Feinstein <asher.feinstein@gmail.com>
2025-09-23 13:47:47 -0400

    Updated Player Bullets
    changed bullet sprite added an animation when they hit and made it so they arn't influenced by player y velocity

--- player Bullet/player_bullet.gd
+++ player Bullet/player_bullet.gd
@@ -4,11 +4,14 @@ extends CharacterBody2D
 @export var bulletSpeed: int = 3000
 @export var additionVelocity: Vector2
 
+@onready var hitWallAnimation = $bulletHitAnimation
+
 func _ready() -> void:
 	visible = false
 	look_at(global_position + direction)
 func _physics_process(delta: float) -> void:
-	velocity = additionVelocity + (direction * bulletSpeed)
+	velocity = (direction * bulletSpeed)
+	velocity.x += additionVelocity.x
 	move_and_slide()
 
 
@@ -18,3 +21,8 @@ func _on_bullet_auto_delete_timeout() -> void:
 #shows when bullet should become visible
 func _on_bullet_visible_in_timeout() -> void:
 	visible = true
+
+
+func _on_bullet_hit_body_entered(body: Node2D) -> void:
+	#activates when bullet hits a wall
+	hitWallAnimation.play("bulletHitWall")
