c74a4f58f6b1b3ae25b5330a4aeaecba691feb31
Asher Feinstein <asher.feinstein@gmail.com>
2025-10-20 11:27:09 -0400

    Basic enemies now can take damage when shot
    Also moved basic idle code to base enemy

--- Enemies/Base Enemy Behaviour/enemy_base.gd
+++ Enemies/Base Enemy Behaviour/enemy_base.gd
@@ -6,14 +6,43 @@ extends CharacterBody2D
 @export var animation:AnimatedSprite2D
 
 #changing variables
+var Enemydirection = Vector2(1,0)
+
 var stunned = false
 func take_damage(amount):
+	#Takes <amount> Health away from enemy
 	health -= amount
-	animation.modulate = Color(1.0, 0.25, 0.25, 1.0)
-	stunned = true
-	await get_tree().create_timer(stunTime, true, false, true).timeout
-	stunned = false
-	animation.modulate = Color(1.0, 1.0, 1.0, 1.0)
+	if health > 0:
+		animation.modulate = Color(1.0, 0.25, 0.25, 1.0)
+		stunned = true
+		await get_tree().create_timer(stunTime, true, false, true).timeout
+		stunned = false
+		animation.modulate = Color(1.0, 1.0, 1.0, 1.0)
+	else:
+		dead()
+
+func idle_movement(speed):
+	#basic movement for enemy
+	#checks RayCasts and Quadrants for obstacles
+	if Enemydirection.x == 1:
+		if directions.QuadrantCollisions["Q4"] == false:
+			Enemydirection.x = -1
+		else:
+			if directions.DirectionCollisions["Right"] != null:
+				Enemydirection.x = -1
+
+	else:
+		if directions.QuadrantCollisions["Q3"] == false:
+			Enemydirection.x = 1
+		else:
+			if directions.DirectionCollisions["Left"] != null:
+				Enemydirection.x = 1
+	velocity = 100 * Enemydirection
+	move_and_slide()
 
 func dead():
 	queue_free()
+
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	take_damage(1)
--- basic_enemy.gd
+++ basic_enemy.gd
@@ -1,22 +1,5 @@
 extends "res://Enemies/Base Enemy Behaviour/enemy_base.gd"
-var Enemydirection = Vector2(1,0)
 
+var SPEED = 200
 func _physics_process(delta: float) -> void:
-	#basic movement for enemy
-	#checks RayCasts and Quadrants for obstacles
-	if Enemydirection.x == 1:
-		if directions.QuadrantCollisions["Q4"] == false:
-			Enemydirection.x = -1
-		else:
-			if directions.DirectionCollisions["Right"] != null:
-				Enemydirection.x = -1
-
-	else:
-		if directions.QuadrantCollisions["Q3"] == false:
-			Enemydirection.x = 1
-		else:
-			if directions.DirectionCollisions["Left"] != null:
-				Enemydirection.x = 1
-	velocity = 100 * Enemydirection
-	move_and_slide()
-	
+	idle_movement(SPEED)
